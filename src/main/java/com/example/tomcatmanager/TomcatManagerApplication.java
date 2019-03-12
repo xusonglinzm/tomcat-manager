aebc76874cebe5505e4fb1f00527536ae35e3e94
xusonglinzm <18842830710@163.com>
2019-03-12 11:11:24 +0800

    首次上传

---
+++ src/main/java/com/example/tomcatmanager/TomcatManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tomcatmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomcatManagerApplication.class, args);
+    }
+
+}
